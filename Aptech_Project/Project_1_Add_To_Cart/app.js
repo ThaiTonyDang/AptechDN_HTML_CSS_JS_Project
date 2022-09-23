a35c4bf923354ae75dee88b09e0dbf8787775872
THAI TRAN <dangtranthaidtv50dh@gmail.com>
2022-09-23 17:14:45 +0700

    rend cart from table

--- Aptech_Project/Project_1_Add_To_Cart/app.js
+++ Aptech_Project/Project_1_Add_To_Cart/app.js
@@ -5,56 +5,56 @@ function Data() {
         {
             "Id": "1",
             "Name": "Mũi Hàn 500",
-            "Price": "25000",
+            "Price": 25000,
             "Count":  1,
             "Image": "product_1.jpg"
         } ,
         {
             "Id": "2",
             "Name": "Trạm Hàn Hakko 942 75W Cực Nóng",
-            "Price": "1299000",
+            "Price": 1299000,
             "Count":  1,
             "Image": "product_2.jpg"
         } ,
         {
             "Id": "3",
             "Name": "Module DIY Sạc Không Dây Q",
-            "Price": "89000 đ",
+            "Price": 89000,
             "Count":  1,
             "Image": "product_3.jpg"
         } ,
         {
             "Id": "4",
             "Name": "Bộ Điều Chỉnh Điện Áp CHLVFU SDTY-200P",
-            "Price": "2199000 đ",
+            "Price": 2199000,
             "Count":  1,
             "Image": "product_4.jpg"
         } ,
         {
             "Id": "5",
             "Name": "Module ISD1760",
-            "Price": "115000 đ",
+            "Price": 115000,
             "Count":  1,
             "Image": "product_5.jpg"
         },
         {
             "Id": "6",
             "Name": "Máy Bơm Chìm Hộ Gia Đình QDX 1500W",
-            "Price": "1720000 đ",
+            "Price": 1720000,
             "Count":  1,
             "Image": "product_6.jpg"
         },
         {
             "Id": "7",
             "Name": "Module Nodemcu IOT ESP8266",
-            "Price": "129000 đ",
+            "Price": 129000,
             "Count": 1,
             "Image": "product_7.jpg"
         },
         {
             "Id": "8",
             "Name": "Raspberry Pi 3 E14 Model B Plus B+",
-            "Price": "1400000 đ",
+            "Price": 1400000,
             "Count": 1,
             "Image": "product_8.jpg"
         }
@@ -69,9 +69,9 @@ function dataLoading() {
   var getDataFromStorage = sessionStorage.getItem("stringList");
   if (getDataFromStorage != null) {
     productArray = JSON.parse(getDataFromStorage);
-  } else {
+} else {
     productArray = Data();
-  }
+}
 
   return productArray;
 }
@@ -93,7 +93,7 @@ function renderData() {
                         <h3 class="content-product-h3">${productArray[i].Name}</h3>
                         <div class="content-product-deltals">
                             <div class="price">
-                                <span class="money">${productArray[i].Price}</span>
+                                <span class="money">${productArray[i].Price} đ</span>
                             </div>
                             <button type="button" class="btn btn-cart" onclick="addToCartAndCalculator(${productArray[i].Id})">ADD TO CART</button>
                         </div>
@@ -106,7 +106,6 @@ function renderData() {
 
 function addToCart(id) {
   var productArray = dataLoading();
-
   for (var i = 0; i < productArray.length; i++) {
     if (productArray[i].Id == id) {
       let isProductExist = false;
@@ -114,17 +113,16 @@ function addToCart(id) {
         for (var j = 0; j < cartArray.length; j++) {
           if (cartArray[j].Id == id) {
             isProductExist = true;
-            productArray[j].Count += 1;
+            productArray[j].Count += 1;        
           }
         }
       }
       if (!isProductExist) {
         cartArray.push(productArray[i]);
       }
+      
     }
   }
-
-  console.log(cartArray);
 }
 
 function calcartQuality() {
@@ -132,7 +130,35 @@ function calcartQuality() {
   document.getElementById("cart__item-number").innerText = cartQuality;
 }
 
+function rendToInventory(id)
+{ 
+  let render = document.getElementById("table-body");
+  render.innerHTML = "";
+  let productRender = "";
+  for(let i = 0 ; i< cartArray.length; i++)
+  {
+    
+      productRender +=
+       ` 
+       <tr style="text-align: center;vertical-align: center;">
+           <td  >${cartArray[i].Id}</td>
+           <td> <img style = "width: 4rem; height: 4rem;" src="./image/${cartArray[i].Image}" alt=""></td>
+           <td>${cartArray[i].Name}</td>
+           <td >${cartArray[i].Price} đ</td>
+           <td id="quantity-${cartArray[i].Id}">${1}</td>
+           <td >${parseInt(cartArray[i].Price)*1} đ</td>            
+      ` 
+  }
+  render.innerHTML = productRender;
+}
+
+function visibilityCart() {
+    document.getElementById("cart__item-number").style.opacity = "1";
+    document.getElementById("cart__item-number").style.visibility = "visible";
+}
 function addToCartAndCalculator(id) {
   addToCart(id);
   calcartQuality();
+  visibilityCart();
+  rendToInventory(id);
 }
